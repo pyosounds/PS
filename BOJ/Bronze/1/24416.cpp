c11f419ccf6827bd5b60700893b518f4ca4ac5fb
pyosounds <govlhappyday@naver.com>
2025-02-12 23:28:41 +0900

    250212

---
+++ BOJ/Bronze/1/24416.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[41];
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 1;
+
+	for (int i = 3; i <= n; ++i)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	cout << dp[n] << ' ' << n - 2;
+
+	return 0;
+}
