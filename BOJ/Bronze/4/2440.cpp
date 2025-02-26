836c6346afbb55645a0d44d63b0a0c1b6522eedb
pyosounds <govlhappyday@naver.com>
2025-02-26 23:33:34 +0900

    250226

---
+++ BOJ/Bronze/4/2440.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = N; j > i; --j)
+		{
+			cout << '*';
+		}
+
+		cout << '\n';
+	}
+
+	return 0;
+}
