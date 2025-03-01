4547facc23f80cda55faa501051be1ae6391cf81
pyosounds <govlhappyday@naver.com>
2025-03-01 23:38:53 +0900

    250301

---
+++ BOJ/Bronze/3/2441.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			cout << ' ';
+		}
+
+		for (int j = i; j < N; ++j)
+		{
+			cout << '*';
+		}
+
+		cout << '\n';
+	}
+
+	return 0;
+}
