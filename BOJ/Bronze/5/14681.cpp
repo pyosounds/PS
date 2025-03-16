c5d46f76ff7db504fd9b444970248414fb236496
pyosounds <govlhappyday@naver.com>
2025-03-16 23:47:31 +0900

    250316

---
+++ BOJ/Bronze/5/14681.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+		
+	int x, y;
+	cin >> x >> y;
+
+	if (x > 0 && y > 0)
+	{
+		cout << 1;
+	}
+	else if (x < 0 && y > 0)
+	{
+		cout << 2;
+	}
+	else if (x < 0 && y < 0)
+	{
+		cout << 3;
+	}
+	else if (x > 0 && y < 0)
+	{
+		cout << 4;
+	}
+
+	return 0;
+}
